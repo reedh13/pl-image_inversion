91b5e3199830038eeeec3b84342093900717ae02
Reed <hansenreed@gmail.com>
2021-04-15 12:31:56 -0400

    First commit

--- image_inversion/image_inversion.py
+++ image_inversion/image_inversion.py
@@ -9,6 +9,24 @@
 #
 
 from chrisapp.base import ChrisApp
+import  os
+from    os                  import  listdir
+from    os                  import  walk
+from    os.path             import  isfile, join, exists
+import  sys
+import  glob
+from PIL import Image, ImageChops
+# import  numpy as np
+# import  re
+# import  time
+# import  pudb
+
+# import  pydicom              as      dicom
+# import  pylab
+# import  matplotlib.cm        as      cm
+# import  pfmisc
+# from    pfmisc._colors      import  Colors
+# from    pfmisc.message      import  Message
 
 
 Gstr_title = r"""
@@ -116,12 +134,85 @@ class Image_inversion(ChrisApp):
         Use self.add_argument to specify a new app argument.
         """
 
+        self.add_argument('-i', '--inputFile',
+                            dest        = 'inputFile',
+                            type        = str,
+                            optional    = False,
+                            help        = 'name of the input file within the inputDir',
+                            default     = ''
+                        )
+        self.add_argument('-t', '--outputFileType',
+                            dest        = 'outputFileType',
+                            type        = str,
+                            default     = '',
+                            optional    = True,
+                            help        = 'output image file format'
+                        )
+
+    
+
     def run(self, options):
         """
         Define the code to be run by this plugin app.
         """
+
+        def mkdir(newdir, mode=0x775):
+            """
+            works the way a good mkdir should :)
+                - already exists, silently complete
+                - regular file in the way, raise an exception
+                - parent directory(ies) does not exist, make them as well
+            """
+            if os.path.isdir(newdir):
+                pass
+            elif os.path.isfile(newdir):
+                raise OSError("a file with the same name as the desired " \
+                            "dir, '%s', already exists." % newdir)
+            else:
+                head, tail = os.path.split(newdir)
+                if head and not os.path.isdir(head):
+                    os.mkdir(head)
+                if tail:
+                    os.mkdir(newdir)
+
         print(Gstr_title)
         print('Version: %s' % self.get_version())
+        print("Options:", options)
+        print("inputdir=", options.inputdir)
+        print(os.getcwd())
+
+        inputFile = options.inputFile
+        inputFilename = inputFile.split('.')[0]
+        inputExtension = inputFile.split('.')[-1]
+
+        # Validate inputFile arg
+        if inputExtension not in ['dcm', 'png', 'jpg', 'jpeg']:
+            raise ValueError("inputFile must be of type .dcm, .png, .jpg, or .jpeg.")
+
+        # Validate outputFileType arg
+        if options.outputFileType == '':
+            outputExtension = inputExtension
+        elif options.outputFileType not in ['.dcm', '.png', '.jpg', '.jpeg']:
+            raise ValueError("outputFileType must be of type .dcm, .png, .jpg, or .jpeg.")
+        else:
+            outputExtension = options.outputFileType
+        
+        # Test if file exists
+        input_path = options.inputdir + '/' + inputFile
+        if not exists(input_path):
+            raise ValueError("File not found: " + input_path)
+
+        # Create output folder
+        mkdir(options.outputdir)
+
+        # Invert and save image
+        # Case for png/jpg/jpeg
+        if inputExtension in ['png', 'jpg', 'jpeg']:
+            inverted_img = ImageChops.invert(Image.open(input_path))
+            inverted_img.save(str(options.outputdir) + '/' + inputFilename + '_inverted' + outputExtension)
+        # Case for dcm
+        else:
+            raise ValueError("DICOM support not implemented yet.")
 
     def show_man_page(self):
         """
